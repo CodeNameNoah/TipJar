a4672e503caea770111e54498745b79f601a8ab7
kylecoding1 <128002901+kylecoding1@users.noreply.github.com>
2023-06-03 17:18:05 -0700

    .

--- client/src/components/ConnectLink.js
+++ client/src/components/ConnectLink.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const ConnectLink = ({ connectToMetaMask }) => {
   return (
-    <a href="#" className="link-button" onClick={connectToMetaMask}>
-      Connect to MetaMask
-    </a>
+    <div>
+      <a href="#" className="link-button" onClick={connectToMetaMask}>
+        Connect to MetaMask
+      </a>
+      <div className="center-container">
+        <a href="https://github.com/CodeNameNoah/TransCrypt#readme" target="_blank" rel="noopener noreferrer" className="help-button">Help?</a>
+      </div>
+    </div>
   );
 };
 
