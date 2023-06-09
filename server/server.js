58f69c30dac1b50488871b6cecf66a93833b5bda
kylecoding1 <128002901+kylecoding1@users.noreply.github.com>
2023-06-09 08:25:59 -0700

    .

--- server/server.js
+++ server/server.js
@@ -2,53 +2,45 @@ const path = require('path');
 const express = require('express');
 const tipRoutes = require('./routes/tipRoutes');
 const { ApolloServer } = require('apollo-server-express');
-const router = express.Router();
-const authenticationMiddleware = require('./middleware/authentication');
 const db = require("./config/connection")
-const authRoutes = require('./routes/authRoutes');
 const {typeDefs, resolvers} = require("./schema")
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-
 // Create Apollo Server
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  introspection: true, // enables introspection
+  playground: true // enables the Playground IDE
+});
 
 // Middleware
 app.use(express.json());
 
-
+// Start the Apollo Server and apply middleware to Express app
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
+
+  // Routes
+  app.use('/api/tips', tipRoutes);
+
+  // Static file serving and catch-all route for React app
+  // These come after all other routes/middleware
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
+    });
+  });
+};
 
-  startApolloServer();
-
-// Start the Apollo Server and apply middleware to Express app
-/*server.start().then(() => {
- server.applyMiddleware({ app });
-});*/
-
-// Routes
-app.use('/api/tips', tipRoutes);
-
-
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
-// Start the server
-/*app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});*/
+startApolloServer();
